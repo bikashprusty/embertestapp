1158aaafd91e2cde24d9007e542e3f6a53f50eb8
bikashprusty <bikash.sandbox@gmail.com>
2018-11-24 17:58:12 +0530

    Ember test cases usage

---
+++ app/routes/index.js
@@ -0,0 +1,16 @@
+import Route from '@ember/routing/route';
+
+export default Route.extend({
+
+    actions: {
+    displayAlert(text) {
+      this._displayAlert(text);
+    }
+  },
+
+  _displayAlert(text) {
+    alert(text);
+  }
+
+  
+});
